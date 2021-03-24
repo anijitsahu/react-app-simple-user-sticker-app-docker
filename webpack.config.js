e16119bda16035c802d1c1abf57b6bef9c6053d8
anijit <anijitsau@gmail.com>
2021-03-24 12:10:44 +0530

    Dependencies are updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-
 module.exports = {
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -12,19 +11,8 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-
+        use: ["style-loader", "css-loader"]
       }
     ]
-  },
-
-  plugins: [
-
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+  }
 };
