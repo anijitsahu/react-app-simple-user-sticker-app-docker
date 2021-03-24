f760ea0cb47d1095aa0467a156904222e682bf6e
anijit <anijitsau@gmail.com>
2021-03-24 12:10:25 +0530

    Dependencies are updated

--- server/dbOps.js
+++ server/dbOps.js
@@ -1,19 +1,17 @@
 // dependencies
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient;
-const ObjectId = mongodb.ObjectID
+const { MongoClient } = require('mongodb');
 
-const URI_TO_CONNECT_MONGODB = "mongodb+srv://root:root123@anijitsmongo-mwm6l.mongodb.net/allapps";
+const URI_TO_CONNECT_MONGODB = "mongodb+srv://root:root123@anijitsmongo-mwm6l.mongodb.net/allapps?writeConcern=majority";
 const DB_NAME = "allapps"
 const COLLECTION_USER_STICKER = "usersticker"
 
 // this function will connect db and based on API send response
-let connectDbAndRunQueries = async (apiName, req, res) => {
+const connectDbAndRunQueries = async (apiName, req, res) => {
 	try {
-		let client = await MongoClient.connect(URI_TO_CONNECT_MONGODB, { useNewUrlParser: true })
+		let client = await new MongoClient(URI_TO_CONNECT_MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }).connect()
 		// select the db, Collections are selected based on needs
+		console.log('CLient as received', client)
 		const db = client.db(DB_NAME)
-
 		// default output
 		const output = { "message": "SUCCESS" }
 
@@ -21,49 +19,47 @@ let connectDbAndRunQueries = async (apiName, req, res) => {
 		chooseApiAndSendResponse(apiName, db, req, res, client, output)
 	} catch (err) {
 		console.log('Some Error occurred ...', err)
+		res.status(503).json({ msg: "Internal Server Error" })
 	}
 }
 
-
 // choose the particular function for an API and process it
-let chooseApiAndSendResponse = (apiName, db, req, res, client, output) => {
+const chooseApiAndSendResponse = (apiName, db, req, res, client, output) => {
 
 	// perform db specific ops based on API names
 	switch (apiName) {
-
 		case 'getAllUsers':
 			makeGetAllUsers(db, req, res, client, output)
 			break;
-
 	}
 }
 
-
-let makeGetAllUsers = async (db, req, res, client, output) => {
+const makeGetAllUsers = async (db, req, res, client, output) => {
 
 	try {
 
 		// db call 
-		let data = await db
+		const data = await db
 			.collection(COLLECTION_USER_STICKER)
 			.find({})
 			.toArray()
 
+		console.log('Data as received', data)
 		output = (data.length > 0) ? [...data] : []
-		sendOutputAndCloseConnection(client, output, res)
-
 	} catch (error) {
 		console.log('unable to get all the users', error)
-		sendOutputAndCloseConnection(client, output, res)
+	} finally {
+		sendResponseAndCloseConnection(client, output, res)
 	}
 }
 
-
 // send the response and close the db connection
-function sendOutputAndCloseConnection(client, output, res) {
+function sendResponseAndCloseConnection(client, output, res) {
 	if (output && res) {
 		console.log(`========================\nOUTPUT AS RECEIVED AND BEFORE SENDING\n==================\n`, output)
-		res.json(output)
+		res.status(200).json(output)
+	} else {
+		res.status(503).json({ msg: "Internal Server Error" })
 	}
 
 	// close the database connection after sending the response
